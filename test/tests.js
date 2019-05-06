3352f75a6901e2f63ec00fe6531fd9bf05dfd6af
DannySuarez <suarezd10@gmail.com>
2019-05-06 16:44:56 -0700

    built and passed test returning user input as an object

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,36 @@
 const test = QUnit.test;
 
-QUnit.testStart(() => {
-    sessionStorage.clear();
-});
+// QUnit.testStart(() => {
+//     sessionStorage.clear();
+// });
 
-test('test', (assert) => {
+function makeUser(formData){
+
+    const user = {
+        name: formData.get('name'),
+        race: formData.get('race'),
+        level: 1,
+        gold: 20
+    };
+    return user;
+}
+
+
+test('return a user as an object', (assert) => {
     // arrange
-    
+    const formData = new FormData();
+    formData.set('name', 'danny');
+    formData.set('race', 'human');
 
+    const expected = {
+        name: 'danny',
+        race: 'human',
+        level: 1,
+        gold: 20
+    };
     // act
-    
+    const user = makeUser(formData)
 
     // assert
-    assert.deepEqual(result, expected);
+    assert.deepEqual(user, expected);
 });
