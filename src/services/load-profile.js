6bd1c9e112bb1f182f81717b4bb72092c752a4ec
DannySuarez <suarezd10@gmail.com>
2019-05-10 09:13:03 -0700

    styled html pages

---
+++ src/services/load-profile.js
@@ -0,0 +1,20 @@
+import api from '../services/api.js';
+
+function loadProfile() {
+    const character = document.getElementById('name');
+    const race = document.getElementById('race');
+    const level = document.getElementById('level');
+    const gold = document.getElementById('gold');
+
+    const user = api.getUser();
+
+    if(!user) {
+        window.location = './';
+    }
+
+    character.textContent = user.name;
+    race.textContent = user.race;
+    level.textContent = user.level;
+    gold.textContent = user.gold;
+}
+export default loadProfile;
