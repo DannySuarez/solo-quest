b7a502ab770701d8bd7b0f8093cb16a6b2edecca
DannySuarez <suarezd10@gmail.com>
2019-05-10 12:49:33 -0700

    took createChoice function out of test

--- test/create-choice.test.js
+++ test/create-choice.test.js
@@ -1,4 +1,5 @@
 import api from '../src/services/api.js';
+import createChoice from '../src/quest/create-choice.js';
 
 const test = QUnit.test;
 
@@ -23,21 +24,3 @@ test('take quest choice object, return it to DOM, append to form', (assert) => {
     assert.deepEqual(dom.outerHTML, expected);
 
 });
-function createChoice(choice) {
-    const label = document.createElement('label');
-    const input = document.createElement('input');
-
-    label.setAttribute('for', 'choice1');
-    input.type = 'radio';
-    input.id = 'choice1';
-    input.name = 'choices';
-    input.value = choice.id;
-    input.required = true;
-
-    label.textContent = choice.description;
-    label.appendChild(input);
-    console.log(label);
-
-    return label;
-
-}
