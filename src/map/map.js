6f0082f73d9936d256c7642dfdcf1fe9c63380ee
DannySuarez <suarezd10@gmail.com>
2019-05-10 09:15:13 -0700

    created loadProfile function to load user profile dynamically

--- src/map/map.js
+++ src/map/map.js
@@ -1,17 +1,4 @@
 import api from '../services/api.js';
+import loadProfile from '../services/load-profile.js';
 
-const character = document.getElementById('name');
-const race = document.getElementById('race');
-const level = document.getElementById('level');
-const gold = document.getElementById('gold');
-
-const user = api.getUser();
-
-if(!user) {
-    window.location = './';
-}
-
-character.textContent = user.name;
-race.textContent = user.race;
-level.textContent = user.level;
-gold.textContent = user.gold;
+loadProfile();
