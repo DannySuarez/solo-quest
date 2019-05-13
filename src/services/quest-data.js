51da1327f78a442ae07d680ce9f9d2fb5c7b22f0
DannySuarez <suarezd10@gmail.com>
2019-05-12 23:18:38 -0700

    added lines for exp

--- src/services/quest-data.js
+++ src/services/quest-data.js
@@ -11,20 +11,23 @@ const newHouse = {
         description: 'Leave the room and explore.',
         result: `After wandering around the house you find the front door and decide to leave`,
         hp: 0,
-        level: 0
+        level: 0,
+        exp: 0
     }, {
         id: 'stay',
         description: 'Stay and play with the toys.',
-        result: `No one ever comes for you and you never leave the room out of fear. 
-                    You die alone but at least you had toys`,
-        hp: -18,
-        level: 0
+        result: `You decide to have some fun and play with the toys. After a few minutes of playing you start to feel ill. 
+                    The toys were a trap! They were poisoned and now your health has gone down.`,
+        hp: -15,
+        level: 0,
+        exp: 0
     }, {
         id: 'cry',
         description: 'Cry and ask for your mom.',
         result: `Your crys draw the attention of monsters who decide they want to have some fun.`,
         hp: -8,
-        level: 0
+        level: 0,
+        exp: 0
     }]
 };
 
@@ -41,20 +44,23 @@ const outside = {
         result: `You attempt to talk to the monster but as soon as you speak it gets frightened.
                     The monster thinks you're a jerk and runs away.`,
         hp: 0,
-        level: 0   
+        level: 0,
+        exp: 0
     }, {
         id: 'scare',
         description: `You're no fool. This monster is faking it and you decide to try and scare it.`,
         result: `You make a mean looking face and raise your arms. The monster runs away in fear and you're a jerk`,
         hp: -5,
-        level: 0
+        level: 0,
+        exp: 1
     }, {
         id: 'attack',
         description: `You go in for an attack. It's either you or him after all.`,
         result: `The monster dies in one punch and drops a picture of his lovely family.
                     You feel terrible for what you've done but you continue on`,
         hp: 0,
-        level: 1
+        level: 1,
+        exp: 2
     }]
 };
 
